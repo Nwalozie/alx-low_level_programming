c64d21b9309ad86e79c687905e5b82e1c50b5418
Nwalozie <iyekcnwalozie@gmail.com>
2023-02-19 13:47:26 -0800

    ment

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+return (0);
+}
+root@6e7b4415d764:/alx-low_level_programming/0x00-hello_world# cat 5-printf.c
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
