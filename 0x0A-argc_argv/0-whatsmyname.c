7a798c491be5cc569ea88669a4fc3586b05da611
Nwalozie <iyekcnwalozie@gmail.com>
2023-03-11 05:15:52 -0800

    batch

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
-* main - Prints the number of arguments passed into it.
-* @argc: Numbers of arguments.
-* @argv: Holds the arguments.
-* Return: 0 to succes.
+* main - Prints the name.
+* @argc: argument count.
+* @argv: argument vector.
+* Return: Always zero.
 */
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
