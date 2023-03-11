18cd469df4075238ee147377941f0d803fe45c58
Nwalozie <iyekcnwalozie@gmail.com>
2023-03-11 05:12:43 -0800

    batch

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
 * Return: 0 to succes.
 */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
