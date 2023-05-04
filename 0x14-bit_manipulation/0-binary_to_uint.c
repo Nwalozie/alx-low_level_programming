669c49c3fc92dd8c2401f1c050ff291d9397b196
Nwalozie <iyekcnwalozie@gmail.com>
2023-05-04 15:53:33 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,74 +1,35 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _pow_recursion - Returns the value of x raised to the power of y.
- * @x: The base.
- * @y: The exponent.
+ * binary_to_uint - converts binary to unsigned int
+ * @b: binary number
  *
- * Return: The value of x raised to the power of y.
- *         If y is lower than 0, return -1.
+ * Return: converted unsigned int
  */
-
-int _pow_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	else if (y > 0)
-		return (x * _pow_recursion(x, y - 1));
-	else
-		return (1 / _pow_recursion(x, -y));
-}
-
-/**
- * _strlen - Returns the length of a string.
- * @s: The string to evaluate.
- *
- * Return: The length of the string.
- */
-
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (s[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: The binary number as a string.
- *
- * Return: The converted number, or 0 if b is NULL or contains non-binary chars.
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0;
-	int len = 0, i = 0, p = 0;
+	unsigned int number = 0;
+	unsigned int bit_value = 1;
+	int index = 0;
 
 	if (b == NULL)
 		return (0);
 
-	len = _strlen(b);
+	while (b[index] != '\0')
+		index++;
 
-	for (i = 0; i < len; i++)
+	index--;
+
+	for (; index >= 0; index--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[index] != '0' && b[index] != '1')
 			return (0);
+
+		if (b[index] == '1')
+			number += bit_value;
+
+		bit_value *= 2;
 	}
 
-	for (i = len - 1; i >= 0; i--)
-	{
-		if (b[i] == '1')
-			sum += _pow_recursion(2, p);
-		p++;
-	}
-
-	return (sum);
+	return (number);
 }
