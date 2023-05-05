35240442fc15e5ad2ca464a26bec2951f744cf37
Nwalozie <iyekcnwalozie@gmail.com>
2023-05-05 16:22:51 -0700

    batch

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_bit - returns the value of a bit.
+ * @n: The value
+ * @index: The index.
+ * Return: The bit.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (n & (1 << index))
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+}
